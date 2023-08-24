f84886148877d03c5a3a1c08cf4c4c30da656564
wbadmin <>
2023-08-24 08:47:21 +0700

    {/src/main/java/com/kie_space/kie_project/Student.java}

--- src/main/java/com/kie_space/kie_project/Student.java
+++ src/main/java/com/kie_space/kie_project/Student.java
@@ -6,12 +6,47 @@ package com.kie_space.kie_project;
 
 public class Student implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Student() {
-    }
+	@org.kie.api.definition.type.Label(value = "id")
+	private java.lang.String id;
+	@org.kie.api.definition.type.Label(value = "name")
+	private java.lang.String name;
+	@org.kie.api.definition.type.Label(value = "result")
+	private java.lang.Boolean result;
 
+	public Student() {
+	}
 
+	public java.lang.String getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.String id) {
+		this.id = id;
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public java.lang.Boolean getResult() {
+		return this.result;
+	}
+
+	public void setResult(java.lang.Boolean result) {
+		this.result = result;
+	}
+
+	public Student(java.lang.String id, java.lang.String name,
+			java.lang.Boolean result) {
+		this.id = id;
+		this.name = name;
+		this.result = result;
+	}
 
 }
